4f057d49d8d26a24740fe65c9d72af8b773c4ff8
filiptomczak <32956874+filiptomczak@users.noreply.github.com>
2020-06-07 12:57:37 +0200

    Update result.m

--- result.m
+++ result.m
@@ -1,5 +1,5 @@
 function[value,x]=result(pop,strl,dim,x_bound)
-[value,k]=min(pop(:,strl*dim+1)); %wybor najwiekszego fitness
+[value,k]=min(pop(:,strl*dim+1)); %choose best fitness
 temp=2.^(strl-1:-1:0)/(2^strl-1); 
 for i=1:dim
     bound(i)=x_bound(i,2)-x_bound(i,1);
